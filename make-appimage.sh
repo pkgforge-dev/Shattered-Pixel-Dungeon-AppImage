03b77b6a11d3c4ad95225cf897384520c0580747
Mechamorph <kazam0180@proton.me>
2026-09-03 07:53:55 +0500

    test

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(curl -s https://api.github.com/repos/00-Evan/shattered-pixel-dungeon/releases/latest | jq -r '.tag_name' | sed 's|^v||')
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://github.com/00-Evan/shattered-pixel-dungeon/blob/master/desktop/src/main/assets/icons/icon_256.png?raw=true
+export DESKTOP=./shatteredpd.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/ShatteredPixelDungeon /usr/lib/app /usr/lib/runtime /usr/lib/libapplauncher.so
 
 # Additional changes can be done in between here
 
