1a8b3aa0e05ee414a1fb674193b932392200c342
Mechamorph <kazam0180@proton.me>
2026-09-03 07:27:10 +0500

    test

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,15 +12,12 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+# Download Shattered-Pixel-Dungeon linux zip
+TAG="$(curl -s https://api.github.com/repos/00-Evan/shattered-pixel-dungeon/releases/latest | jq -r '.tag_name')"
+echo "Downloading Shattered-Pixel-Dungeon $TAG..."
+wget --retry-connrefused --tries=30 https://github.com/00-Evan/shattered-pixel-dungeon/releases/download/$TAG/ShatteredPD-$TAG-Linux.zip -O /tmp/spd.zip
+mkdir -p /tmp/spd
+(cd /tmp/spd; unzip /tmp/spd.zip; ls /tmp/spd; ls /tmp/spd/*)
+exit 1
+sudo cp -r /tmp/wine-x86/* /usr/
+rm -rf /tmp/wine*
